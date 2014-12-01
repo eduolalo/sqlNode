89a3d2844716fa8d0e10bbbf33391132c3057b56
Christian <christian@lastroom.mx>
2014-12-01 00:55:57 -0600

    init

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -1,34 +1,40 @@
 #!/usr/bin/env node
 
-var dataBase = require(process.cwd() + '/lib/dataBase');
+var dataBase = require(process.cwd() + '/lib/dataBase'),
+    sequelize = dataBase.sequelize;
 
-var Manager = dataBase.Manager,
-    App = dataBase.App;
+var writeData = function() {
 
-// Create manager
-var manager = Manager.build({
-    name: 'Christian',
-    email: 'rcchristian@gmail.com'
-});
-manager.save().success(function() {
-    console.log('Manager created: ', manager);
+    var Manager = dataBase.Manager,
+        App = dataBase.App;
 
-    // Create app
-    var app = App.build({
-        name: 'Initial app'
+    // Create manager
+    var manager = Manager.build({
+        name: 'Christian',
+        email: 'rcchristian@gmail.com'
     });
-    app.save().success(function() {
-        console.log('App created: ', app);
+    manager.save().success(function() {
+        console.log('Manager created: ', manager);
 
-        // Setting assosiations
-        manager.setApps([app]).success(function() {
-            console.log('Initial values created!');
+        // Create app
+        var app = App.build({
+            name: 'Initial app'
+        });
+        app.save().success(function() {
+            console.log('App created: ', app);
+
+            // Setting assosiations
+            manager.setApps([app]).success(function() {
+                console.log('Initial values created!');
+            });
+
+        }).error(function(appError) {
+            console.log('Cant create app: ', appErr);
         });
 
-    }).error(function(appError) {
-        console.log('Cant create app: ', appErr);
+    }).error(function(managerErr) {
+        console.log('Cant create manager: ', managerErr);
     });
+}
 
-}).error(function(managerErr) {
-    console.log('Cant create manager: ', managerErr);
-});
+sequelize.sync().success(writeData);
